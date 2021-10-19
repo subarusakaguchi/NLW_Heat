2103884b8b9d60e084d0c421e0bfde93f034bb2e
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-10-19 19:06:56 -0300

    Base do projeto feita

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import * as express from 'express';
+import "dotenv/config"
+import { router } from './routes';
+const app = express();
+const PORT = 4000;
+
+app.use(express.json())
+app.use(router)
+
+app.get('/github', (req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+
+app.get('/signin/callback', (req, res) => {
+  const { code } = req.query;
+  
+  return res.json(code);
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+interface IAccessTokenResponse {
+  access_token: String
+}
+
+interface IUserResponse {
+  avatar_url: String,
+  login: String,
+  id: Number,
+  name: String
+}
+
+class AuthenticateUserService {
+  async execute(code:String) {
+    const url = "https://github.com/login/oauth/access_token"
+
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code
+      },
+
+      headers:{
+        "Accept": "application/json"
+      }
+    })
+
+    const response = await axios.get('https://api.github.com/user', {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`
+      }
+    })
+
+    return response.data;
+  }
+};
+
+export { AuthenticateUserService };
