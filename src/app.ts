4d666811830744194755e1b6dd02d0e6a20831a2
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-10-20 00:42:29 -0300

    Aula 1 Concluída

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
-import * as express from 'express';
+import express from 'express';
 import "dotenv/config"
 import { router } from './routes';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
 const app = express();
-const PORT = 4000;
+
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", socket => console.log(`Usuário conectado no socket ${socket.id}`))
 
 app.use(express.json())
 app.use(router)
@@ -17,4 +32,4 @@ app.get('/signin/callback', (req, res) => {
   return res.json(code);
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+export { serverHttp, io };
--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -5,9 +5,13 @@ class AuthenticateUserController {
   async handle(req: Request, res: Response) {
     const { code } = req.body;
     const service = new AuthenticateUserService;
-    const result = await service.execute(code);
 
-    return res.json(result);
+    try {
+      const result = await service.execute(code);
+      return res.json(result);
+    }catch (err) {
+      return res.json({error: err.message});
+    }    
   };
 };
 
