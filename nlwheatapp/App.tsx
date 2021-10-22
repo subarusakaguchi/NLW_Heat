4a70b6e7dbe1c14bb9eb903d3e6cbea174c3abbb
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-10-22 18:15:38 -0300

    1/3 da aula 3

---
+++ nlwheatapp/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+import AppLoading from 'expo-app-loading';
+import { Home } from './src/screens/Home/index'
+import { StatusBar } from 'expo-status-bar'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <>
+    <StatusBar style='light' />
+    <Home />
+    </>
+  );
+}
